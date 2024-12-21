792aa02e05b18d759933950589f2f186cb44067d
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2024-12-20 22:26:15 -0800

    fewer packages

--- files/scripts/basefromdnf5.sh
+++ files/scripts/basefromdnf5.sh
@@ -7,4 +7,4 @@ set -oue pipefail
 curl -Lo /etc/yum.repos.d/negativo17-fedora-multimedia.repo https://negativo17.org/repos/fedora-multimedia.repo
 sed -i '0,/enabled=1/{s/enabled=1/enabled=1\npriority=90/}' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 
-dnf5 install -y adw-gtk3-theme gnome-tweaks gnome-shell gdm mesa-dri-drivers mesa-libEGL mesa-vulkan-drivers systemd-oomd-defaults tracker tracker-miners plymouth-system-theme polkit nautilus ptyxis gnome-session-wayland-session gnome-color-manager gnome-control-center dconf adobe-source-code-pro-fonts NetworkManager-openvpn-gnome gnome-settings-daemon papirus-icon-theme
+dnf5 install -y --setopt=install_weak_deps=False adw-gtk3-theme gnome-tweaks gnome-shell gdm mesa-dri-drivers mesa-libEGL mesa-vulkan-drivers systemd-oomd-defaults tracker tracker-miners plymouth-system-theme polkit nautilus ptyxis gnome-session-wayland-session gnome-color-manager gnome-control-center dconf adobe-source-code-pro-fonts NetworkManager-openvpn-gnome gnome-settings-daemon papirus-icon-theme
