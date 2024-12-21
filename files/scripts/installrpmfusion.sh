e265b77744db6d8a7b813f4baf9615cfc1f6e022
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2024-12-21 00:38:35 -0800

    rpmfusion

--- files/scripts/installrpmfusion.sh
+++ files/scripts/installrpmfusion.sh
@@ -5,3 +5,8 @@ set -oue pipefail
 
 rpm -q rpmfusion-free-release || rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${OS_VERSION}.noarch.rpm
 rpm -q rpmfusion-nonfree-release || rpm-ostree install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${OS_VERSION}.noarch.rpm
+
+rpm-ostree install unrar
+
+rpm-ostree uninstall rpmfusion-free-release
+rpm-ostree uninstall rpmfusion-nonfree-release
