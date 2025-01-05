c33cf0526c6e83032beaa0ac1ab326cb47d578dd
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-01-04 20:47:10 -0800

    fixes

--- files/scripts/selinux/chromium/chromium.sh
+++ files/scripts/selinux/chromium/chromium.sh
@@ -5,6 +5,7 @@ echo "Building and Loading Policy"
 set -x
 
 make -f /usr/share/selinux/devel/Makefile chromium.pp || exit
+semodule -d chrome
 /usr/sbin/semodule -i chromium.pp -X 600
 
 /sbin/restorecon -F -R -v /usr/lib/chromium-browser/
