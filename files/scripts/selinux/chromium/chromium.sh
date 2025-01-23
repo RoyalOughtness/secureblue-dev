b774120ce0759dcbfe9fedea48db89979d75517d
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-01-23 12:10:41 -0800

    verbose logs

--- files/scripts/selinux/chromium/chromium.sh
+++ files/scripts/selinux/chromium/chromium.sh
@@ -5,7 +5,7 @@ echo "Building and Loading Policy"
 set -x
 
 make -f /usr/share/selinux/devel/Makefile chromium.pp || exit
-/usr/sbin/semodule -i chromium.pp -X 600
+/usr/sbin/semodule -i -v chromium.pp -X 600
 
 /sbin/restorecon -F -R -v /usr/lib/chromium-browser/
 /sbin/restorecon -F -R -v /usr/lib/trivalent/
