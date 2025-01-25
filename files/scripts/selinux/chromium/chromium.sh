2623dbcfaa95cb4329d710510a338c49f44b4655
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-01-25 14:41:58 -0800

    rebuild

--- files/scripts/selinux/chromium/chromium.sh
+++ files/scripts/selinux/chromium/chromium.sh
@@ -5,7 +5,7 @@ echo "Building and Loading Policy"
 set -x
 
 make -f /usr/share/selinux/devel/Makefile chromium.pp || exit
-/usr/sbin/semodule -i chromium.pp -X 600 -v 
+/usr/sbin/semodule -i chromium.pp -v 
 
 /sbin/restorecon -F -R -v /usr/lib/chromium-browser/
 /sbin/restorecon -F -R -v /usr/lib/trivalent/
