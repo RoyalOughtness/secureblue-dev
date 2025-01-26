2788dd4e42d73106435c8b2aa798ad5082606f72
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-01-26 13:47:31 -0800

    move script

--- files/scripts/selinux/chromium/chromium.sh
+++ files/scripts/selinux/chromium/chromium.sh
@@ -7,5 +7,4 @@ set -x
 make -f /usr/share/selinux/devel/Makefile chromium.pp || exit
 /usr/sbin/semodule -i chromium.pp -v 
 
-/sbin/restorecon -F -R -v /usr/lib/chromium-browser/
 /sbin/restorecon -F -R -v /usr/lib/trivalent/
