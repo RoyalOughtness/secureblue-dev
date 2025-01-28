0ef23e958041ff07c55403e66acb3e4405996847
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-01-28 00:22:44 -0800

    fix

--- files/scripts/hardenuserns.sh
+++ files/scripts/hardenuserns.sh
@@ -5,16 +5,16 @@ set -oue pipefail
 
 rpm-ostree install selinux-policy-devel
 
+cd ./selinux/chromium
+bash chromium.sh
+cd ../..
+
 cd ./selinux/flatpakfull
 bash flatpakfull.sh
 cd ../..
 
 cd ./selinux/nautilus
 bash nautilus.sh
-cd ../..
-
-cd ./selinux/chromium
-bash chromium.sh
 cd ../..
 
 semodule -i ./selinux/user_namespace/grant_userns.cil
