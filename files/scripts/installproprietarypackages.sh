9dc97df3cc40d126377f0edd206c91803d94fe36
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-02-22 20:17:07 -0800

    fix

--- files/scripts/installproprietarypackages.sh
+++ files/scripts/installproprietarypackages.sh
@@ -6,7 +6,6 @@ set -oue pipefail
 curl -Lo /etc/yum.repos.d/negativo17-fedora-multimedia.repo https://negativo17.org/repos/fedora-multimedia.repo
 sed -i '0,/enabled=1/{s/enabled=1/enabled=1\npriority=90/}' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 
-rpm-ostree uninstall mesa-libglapi
 
 rpm-ostree override remove \    
     libva \
