0699c246cea6de21615dcebf2243756056d23a51
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-06-03 11:36:48 -0700

    chore: cleanup UX for autounlock script

--- files/system/usr/libexec/luks-enable-tpm2-autounlock.sh
+++ files/system/usr/libexec/luks-enable-tpm2-autounlock.sh
@@ -22,7 +22,7 @@ set -eou pipefail
 echo "WARNING: Do NOT use this if your CPU is vulnerable to faulTPM!"
 echo "All AMD Zen2 and Zen3 Processors are known to be affected!"
 echo "All AMD Zen1 processors are also likely affected, with Zen4 unknown!"
-echo "If you have an AMD CPU, you likely shouldn't use this!"
+echo "If you have an AMD CPU with an fTPM, you shouldn't use this!"
 echo "----------------------------------------------------------------------------"
 echo "This script uses systemd-cryptenroll to enable TPM2 auto-unlock."
 echo "You can review systemd-cryptenroll's manpage for more information."
@@ -63,13 +63,6 @@ else
   exit 1
 fi
 
-SET_PIN='no'
-read -p "Would you like to set a PIN? (y/N): " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  SET_PIN='yes'
-fi
-
 # Specify Crypt Disk by-uuid
 CRYPT_DISK="/dev/disk/by-uuid/$DISK_UUID"
 
@@ -99,7 +92,7 @@ fi
 
 ## Run crypt enroll
 echo "Enrolling TPM2 unlock requires your existing LUKS2 unlock password"
-systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=7+14 --tpm2-with-pin="$SET_PIN" "$CRYPT_DISK"
+systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=7+14 --tpm2-with-pin=yes "$CRYPT_DISK"
 
 # Sets the new tpm keyslot as preferred if it's the only one currently configured. (Users with more than one configured are presumed advanced and capable of their own priority management. Not certain how or why you'd have more than one tpm2 keyslot regardless.)
 if [ "$(echo "$CRYPT_DISK_INFO" | grep -c "systemd-tpm2")" -eq "1" ]; then
